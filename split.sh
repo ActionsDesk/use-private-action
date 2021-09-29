c8d8fe3ae8ec2de626e5c20a98c88d800857977c
Bassem Dghaidi <link-@github.com>
2021-09-29 16:01:16 +0200

    Parse inputs and set env vars

---
+++ split.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+read -d '' INPUT_VARIABLES <<EOF
+username: bassem
+ some other variable: lorem ipsum
+password: 12345678
+thirdvariable:44124124
+EOF
+# Split $INPUT by newline
+readarray -t INPUT_ARRAY <<<"${INPUT_VARIABLES}"
+# Print the array
+for i in "${INPUT_ARRAY[@]}"; do
+  # split the variable $i by colon
+  IFS=":" read -ra KEY_VALUES <<< "${i// /}"
+  # Print the variable name and value
+  echo "key: ${KEY_VALUES[0]}"
+  echo "value: ${KEY_VALUES[1]}"
+  # uppercase the variable name
+  export "INPUT_${KEY_VALUES[0]^^}"="${KEY_VALUES[1]}"
+done
